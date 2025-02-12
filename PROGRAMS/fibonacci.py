7caa3372ab8c89635f07e02138dc19b488a57ae7
charannn <charanbhavya4479@gmail.com>
2025-02-12 15:25:28 +0530

    Add files via upload

--- PROGRAMS/fibonacci.py
+++ PROGRAMS/fibonacci.py
@@ -1,16 +1,11 @@
-a, b = 0, 1
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        print(a, end=' ')
+        a, b = b, a + b
+
+
 n = int(input("Enter the number of terms: "))
-if n == 1:
-print(a)
-elif n == 2:
-print(a)
-print(b)
-else:
-print(a)
-print(b)
-count = 2
-while count < n:
-c = a + b
-print(c)
-a, b = b, c
-count += 1
+fibonacci(n)
+
+
